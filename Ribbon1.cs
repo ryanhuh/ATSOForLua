a28846ab7c9ba8c8d03ba07d53d4b05d77d70768
ryangrav <ryangrav@gravity.co.kr>
2022-04-19 15:31:08 +0900

    lua 파일 읽어서 엑셀 표현

--- Ribbon1.cs
+++ Ribbon1.cs
@@ -46,7 +46,8 @@ namespace ExcelAddIn1
 #if _FILELOAD //그대로 읽기
                 fileLoader.loadLua(app);
 #else
-                fileLoader.loadDataToMemByRegex(app);
+                fileLoader.loadDataToExcelByRegex(app);
+                //fileLoader.loadDataToMemByRegex(app);
 #endif
                 // This code is used to call Close() method Thread safe otherwise you get Cross-thread operation not valid Exception
                 ThreadSafeHelper.InvokeControlMethodThreadSafe(loader, () =>
--- Utils/FileLoader.cs
+++ Utils/FileLoader.cs
@@ -12,6 +12,7 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using System.Text.RegularExpressions;
+using System.Dynamic;
 
 namespace ExcelAddIn1.Utils
 {
@@ -120,20 +121,26 @@ namespace ExcelAddIn1.Utils
 
         }
 
-        public void loadDataToMemByRegex(Excel.Application app)
+
+
+
+        public void loadDataToExcelByRegex(Excel.Application app)
         {
             try
             {
+                Excel.Worksheet sheet = app.ActiveSheet;
                 var filePath = openDlg.FileName;
-                using (Stream str = openDlg.OpenFile())
+                Stream str = openDlg.OpenFile();
+                if(str.Length > 0)
                 {
                     TextReader tr = new StreamReader(str);
                     string text;
-                    int lineNum = 1;                    
+                    int lineNum = 1;
                     //string groupData = "";
                     string titleName = "";
                     string wholeString = "";
-                    Dictionary<string, string> _dicEle = new Dictionary<string, string>();
+
+                    Dictionary<string, IDictionary<string, Object>> _dicEle = new Dictionary<string, IDictionary<string, Object>>();
 
                     wholeString = tr.ReadToEnd();
 
@@ -142,9 +149,9 @@ namespace ExcelAddIn1.Utils
                     Regex r = new Regex(patternSkill, RegexOptions.IgnoreCase);
                     Match mSkill = r.Match(wholeString);
                     int matchCount = 0;
-                    while(mSkill.Success)
+                    while (mSkill.Success)
                     {
-                        //for (int n = 0; n < mSkill.Groups.Count; n++)
+                        var tObj = new ExpandoObject() as IDictionary<string, Object>;
                         {
                             Group gD = mSkill.Groups[0];
                             Debug.WriteLine("Match Count = " + (++matchCount));
@@ -160,18 +167,191 @@ namespace ExcelAddIn1.Utils
                                 {
                                     titleName = mName.Value.Replace("[", "").Replace("]", "");
                                 }
+
+                                String[] elementString = new string[50];
+
                                 string patternKeyValue = @"(\S+)\s*=\s*(.*),(\s*--.*)*\r?\n";
+                                //string patternKeyValue = @"(\S+)\s*=\s*(.*)\r?\n";
+                                Regex element = new Regex(patternKeyValue, RegexOptions.IgnoreCase);
+                                Match eleM = element.Match(groupData);
+                                int eleCount = 0;
+                                while (eleM.Success)
+                                {
+                                    if (eleM.Groups.Count >= 3)
+                                    {
+                                        Group eD = eleM.Groups[0];
+                                        //Debug.WriteLine("Ele Match Count = " + (++eleCount));
+                                        Debug.WriteLine(eD);
+                                        if (!tObj.ContainsKey(eleM.Groups[1].ToString()))
+                                        {
+                                            tObj.Add(eleM.Groups[1].ToString(), eleM.Groups[2]);
+                                        }
+                                    }
+
+                                    eleM = eleM.NextMatch();
+                                }
+                                if(!_dicEle.ContainsKey(titleName))
+                                    _dicEle.Add(titleName, tObj);
+
+                                int cellNumbering = _dicEle.Count();
+
+                                if (cellNumbering == 1)//Header를 만든다.
+                                {
+                                    //Skill Name
+                                    Excel.Range cT1 = sheet.Cells[cellNumbering, 1];
+                                    Excel.Range cT2 = sheet.Cells[cellNumbering, 1];
+                                    Excel.Range rng = sheet.get_Range(cT1, cT2);
+                                    rng.Value2 = @"스킬이름";
+
+                                    cT1 = sheet.Cells[cellNumbering+1, 1];
+                                    cT2 = sheet.Cells[cellNumbering+1, 1];
+                                    rng = sheet.get_Range(cT1, cT2);
+                                    rng.Value2 = titleName;
+
+                                    int cellCount = 2;
+                                    foreach (var entry in tObj)
+                                    {
+                                        Excel.Range cE1 = sheet.Cells[cellNumbering, cellCount];
+                                        Excel.Range cE2 = sheet.Cells[cellNumbering, cellCount];
+                                        Excel.Range rngE = sheet.get_Range(cE1, cE2);
+
+                                        rngE.Value2 = entry.Key.ToString();
+
+                                        cE1 = sheet.Cells[cellNumbering+1, cellCount];
+                                        cE2 = sheet.Cells[cellNumbering+1, cellCount];
+                                        rngE = sheet.get_Range(cE1, cE2);
+
+                                        rngE.Value2 = entry.Value.ToString();
+
+                                        ++cellCount;
+                                    }
+
+
+
+                                }
+                                else
+                                {// Header를 찾아서 값을 넣어 준다.
+                                    //Skill Name
+                                    Excel.Range cT1 = sheet.Cells[cellNumbering, 1];
+                                    Excel.Range cT2 = sheet.Cells[cellNumbering, 1];
+                                    Excel.Range rng = sheet.get_Range(cT1, cT2);
+                                    rng.Value2 = titleName;
+
+                                    int cellCount = 2;
+                                    foreach (var entry in tObj)
+                                    {
+                                        Excel.Range cE1 = sheet.Cells[cellNumbering, cellCount];
+                                        Excel.Range cE2 = sheet.Cells[cellNumbering, cellCount];
+                                        Excel.Range rngE = sheet.get_Range(cE1, cE2);
+
+                                        rngE.Value2 = entry.Value.ToString();
+                                        ++cellCount;
+                                    }
+                                }
+                            }
+                        }
+                        if (matchCount > 343)
+                            break;
+
+                        mSkill = mSkill.NextMatch();
+                        app.StatusBar = string.Format("loading {0}", matchCount);
+                    }
+                }
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                $"Details:\n\n{ex.StackTrace}");
+            }
+
+        }
+
+        public void loadDataToMemByRegex(Excel.Application app)
+        {
+            try
+            {
+                var filePath = openDlg.FileName;
+                using (Stream str = openDlg.OpenFile())
+                {
+                    TextReader tr = new StreamReader(str);
+                    string text;
+                    int lineNum = 1;                    
+                    //string groupData = "";
+                    string titleName = "";
+                    string wholeString = "";
+                    //Dictionary<string, string[]> _dicEle = new Dictionary<string, string[]>();
+
+                    
+                    Dictionary<string, IDictionary<string, Object>> _dicEle = new Dictionary<string, IDictionary<string, Object>>();
+
+                    wholeString = tr.ReadToEnd();
+
+                    string patternSkill = @"Add\(\s*\[\[(\S+)\]\],(\s*--.*)*\r?\n\s*\{\r?\n(\s*((?!\s*\}).*\r?\n)*)\s*\}\s*\);\r?\n";
+                    //스킬 단위로 분리
+                    Regex r = new Regex(patternSkill, RegexOptions.IgnoreCase);
+                    Match mSkill = r.Match(wholeString);
+                    int matchCount = 0;
+                    while(mSkill.Success)
+                    {
+                        //for (int n = 0; n < mSkill.Groups.Count; n++)
+                        var tObj = new ExpandoObject() as IDictionary<string, Object>;
+                        {
+                            Group gD = mSkill.Groups[0];
+                            Debug.WriteLine("Match Count = " + (++matchCount));
+                            Debug.WriteLine(gD);
+                            CaptureCollection cc = gD.Captures;
+                            for (int j = 0; j < cc.Count; j++)
+                            {
+                                Capture cData = cc[j];
+                                string groupData = cData.ToString();
+                                string patternName = @"\[\[([^\]]+)\]";
+                                Match mName = Regex.Match(groupData, patternName);
+                                if (mName.Success)
+                                {
+                                    titleName = mName.Value.Replace("[", "").Replace("]", "");
+                                }
+
+                                //Dynamic Object 생성
+                                //tObj.Add(titleName, titleName);
+                                
+
+                                //
+                                String[] elementString = new string[50];
+
+                                //string patternKeyValue = @"(\S+)\s*=\s*(.*),(\s*--.*)*\r?\n";
+                                string patternKeyValue = @"(\S+)\s*=\s*(.*)\r?\n";
                                 Regex element = new Regex(patternKeyValue, RegexOptions.IgnoreCase);
                                 Match eleM = element.Match(groupData);                                
                                 int eleCount = 0;
-                                while(eleM.Success)
+                                while (eleM.Success)
                                 {
-                                    Group eD = eleM.Groups[0];
-                                    Debug.WriteLine("Ele Match Count = " + (++eleCount));
-                                    Debug.WriteLine(eD);
+                                    if (eleM.Groups.Count >= 3) { 
+                                        Group eD = eleM.Groups[0];
+                                        //Debug.WriteLine("Ele Match Count = " + (++eleCount));
+                                        Debug.WriteLine(eD);
+                                        tObj.Add(eleM.Groups[1].ToString(), eleM.Groups[2]);
+                                     }
+                                    
+                                    //elementString[eleCount++] = eD.ToString();
+                                    /*CaptureCollection ccc = eD.Captures;
+                                    if (ccc.Count > 1)
+                                    {
+                                        Capture cap1 = ccc[0];
+                                        Capture cap2 = ccc[0];
+                                        Debug.WriteLine(string.Format("capture = {0} : {1}", cap1, cap2));
+                                        //tObj.Add(string.Format("{0}", eD.Value, Name);
+                                    }*/
+
+                                    //Debug.WriteLine(Regex.Replace(eD.Value, patternKeyValue, m => string.Format("{0}:{1}", m.Value[0], m.Value[1])));
+                                    //string eleString = eD.Value;
+                                    //Debug.WriteLine("{0} ", eleString.Substring(0, eD.Index));
+
                                     eleM = eleM.NextMatch();
                                 }
+                                _dicEle.Add(titleName, tObj);
+
                                 //Debug.WriteLine(Regex.Replace(groupData, patternKeyValue, m => string.Format("{0}{1}", m.Value, m.Value)));
+                                //_dicEle.Add(titleName, elementString);
                             }
                         }
                         mSkill = mSkill.NextMatch();
